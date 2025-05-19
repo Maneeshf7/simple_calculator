cef30691e239c837afbc256be59fe7de6555783a
l.Maneesh <lmaneesh887@gmail.com>
2025-05-19 20:08:03 +0530

    Create simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+op=input("enter the operator +,-,*,/: ")
+
+num1=float(input("enter the number :"))
+num2=float(input("enter the number :" ))
+if op=="+":
+    result=num1+num2
+    print(result)
+elif op=="-":
+    result=num1-num2
+    print(result)
+elif op=="*":
+    result=num1*num2
+    print(result)
+elif op=="/":
+    result=num1/num2
+    print(result)
